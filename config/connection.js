0ee074879404755061ce2a01e60bf2fc671e291a
Carlee Miller <carlee.miller@gmail.com>
2023-03-20 11:33:52 -0500

    added .env file for security and updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "",
-    database: "employees",
+    database: process.env.DB_NAME,
+    user: process.env.USER,
+    password: process.env.PASSWORD, 
 });
 
 connection.connect(function(err) {
