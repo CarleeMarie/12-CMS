5a0a1ca122c81c658b8a88fc614653f0470fbef2
Carlee Miller <carlee.miller@gmail.com>
2023-03-20 22:56:48 -0500

    just finished tutoring session with Juan Delgado - we went over most of the files and started running it. We found errors and were working on resolving them when our time was up. Will pick up on error messages tomorrow.

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,36 @@
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE IF NOT EXISTS company_db;
  
 USE company_db;
 
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     dept_name VARCHAR(30) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    dept_id INT NOT NULL,
-    PRIMARY KEY(id)
+    dept_id INT,
+    PRIMARY KEY(id),
     FOREIGN KEY (dept_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (manager_id)
-    REFERENCES role(title) 
+    REFERENCES employee(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
     ON DELETE SET NULL
-    INDEX manager_id (manager_id),
-    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,10 @@
-use employees;
+use company_db;
 
 INSERT INTO department
     (dept_name)
 VALUES
-    ('Store Director'),
-    ('Executive Team Lead'),
-    ('Team Lead'),
+    ('General Management'),
+    ('Team Leaders'),
     ('Team Member');
 
 INSERT INTO role
@@ -14,15 +13,15 @@ VALUES
     ('Store Director', 150000, 1),
     ('Executive Team Lead', 80000, 2),
     ('Team Lead', 50000, 3),
-    ('Front End', 25000, 4),
-    ('Fulfillment', 25000, 5),
-    ('Beauty', 25000, 6),
-    ('Style', 25000, 7),
-    ('Grocery', 25000, 8),
-    ('Domestics', 25000, 9),
-    ('Seasonal', 25000, 10),
-    ('Electronics', 25000, 11),
-    ('Freight', 25000, 12);
+    ('Front End', 25000, 4);
+    -- ('Fulfillment', 25000, 5),
+    -- ('Beauty', 25000, 6),
+    -- ('Style', 25000, 7),
+    -- ('Grocery', 25000, 8),
+    -- ('Domestics', 25000, 9),
+    -- ('Seasonal', 25000, 10),
+    -- ('Electronics', 25000, 11),
+    -- ('Freight', 25000, 12);
 
 INSERT INTO employee
     (first_name, last_name, role_id, department, salary, manager_id)
@@ -30,12 +29,12 @@ VALUES
     ('Raymond', 'Schnuller', 1),
     ('Brandon', 'Puppe', 2),
     ('Lisa', 'Hartung', 3),
-    ('Brett', 'Haisha', 4),
-    ('Heather', 'Simpson', 5),
-    ('Jenna', 'Doberman', 6),
-    ('Carlton', 'Verruckt', 7),
-    ('Alec', 'Verlierer', 8),
-    ('Tony', 'Schumacher', 9),
-    ('Steve', 'Martin', 10),
-    ('Brandon', 'Smoody', 11);
+    ('Brett', 'Haisha', 4);
+    -- ('Heather', 'Simpson', 5),
+    -- ('Jenna', 'Doberman', 6),
+    -- ('Carlton', 'Verruckt', 7),
+    -- ('Alec', 'Verlierer', 8),
+    -- ('Tony', 'Schumacher', 9),
+    -- ('Steve', 'Martin', 10),
+    -- ('Brandon', 'Smoody', 11);
 
