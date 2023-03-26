e646a9062fd34cfd264a9fe0cad5b11a4eeff870
Carlee Miller <carlee.miller@gmail.com>
2023-03-26 11:31:07 -0500

    fixed and updated seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -5,36 +5,36 @@ INSERT INTO department
 VALUES
     ('General Management'),
     ('Team Leaders'),
-    ('Team Member');
+    ('Team Members');
 
 INSERT INTO role
     (title, salary, dept_id)
 VALUES
     ('Store Director', 150000, 1),
-    ('Executive Team Lead', 80000, 2),
-    ('Team Lead', 50000, 3),
-    ('Front End', 25000, 4);
-    -- ('Fulfillment', 25000, 5),
-    -- ('Beauty', 25000, 6),
-    -- ('Style', 25000, 7),
-    -- ('Grocery', 25000, 8),
-    -- ('Domestics', 25000, 9),
-    -- ('Seasonal', 25000, 10),
-    -- ('Electronics', 25000, 11),
-    -- ('Freight', 25000, 12);
+    ('Executive Team Lead', 80000, 1),
+    ('Team Lead', 50000, 2),
+    ('Front End', 25000, 3);
+    ('Fulfillment', 25000, 3),
+    ('Beauty', 25000, 3),
+    ('Style', 25000, 3),
+    ('Grocery', 25000, 3),
+    ('Domestics', 25000, 3),
+    ('Seasonal', 25000, 3),
+    ('Electronics', 25000, 3),
+    ('Freight', 25000, 3);
 
 INSERT INTO employee
     (first_name, last_name, role_id, department, salary, manager_id)
 VALUES
     ('Raymond', 'Schnuller', 1),
-    ('Brandon', 'Puppe', 2),
-    ('Lisa', 'Hartung', 3),
-    ('Brett', 'Haisha', 4);
-    -- ('Heather', 'Simpson', 5),
-    -- ('Jenna', 'Doberman', 6),
-    -- ('Carlton', 'Verruckt', 7),
-    -- ('Alec', 'Verlierer', 8),
-    -- ('Tony', 'Schumacher', 9),
-    -- ('Steve', 'Martin', 10),
-    -- ('Brandon', 'Smoody', 11);
+    ('Brandon', 'Puppe', 1),
+    ('Lisa', 'Hartung', 2),
+    ('Brett', 'Haisha', 2);
+    ('Heather', 'Simpson', 2),
+    ('Jenna', 'Doberman', 2),
+    ('Carlton', 'Verruckt', 3),
+    ('Alec', 'Verlierer', 2),
+    ('Tony', 'Schumacher', 2),
+    ('Steve', 'Martin', 3),
+    ('Brandon', 'Smoody', 3);
 
